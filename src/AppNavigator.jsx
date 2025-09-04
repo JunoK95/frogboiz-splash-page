c530b4fd65dfc78aa0266704377214acca96d6c0
JunoK95 <junokimemail@gmail.com>
2025-09-03 22:15:34 -0600

    Fix router to use hashrouter

--- src/AppNavigator.jsx
+++ src/AppNavigator.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
 import {
-  BrowserRouter as Router, Redirect, Route, Switch,
-} from 'react-router-dom';
-import HomePage from './pages/HomePage';
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function AppNavigator() {
   return (
